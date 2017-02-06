7de5e27852bf6a7cba9bcab3a84506cecc286549
Jan Hermann <hermajan@gmail.com>
2017-02-06 03:15:29 +0100

    Added icons and styles.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,64 +1,59 @@
 class ReservationsController < ApplicationController
-    before_action :authenticate_user!
-  
-  def index
-    @reservations = Reservation.all.order(:id)
-    #@important = Task.where("priority < '0'").order(:priority).all
-  end
+		before_action :authenticate_user!
+	
+	def index
+        if current_user.try(:admin?)
+		    @reservations = Reservation.all.order(:id)
+        else
+            @reservations = Reservation.where('user_id' => current_user.id).order(:id)
+		end   
+	end
 
-  def show
-    @reservation = Reservation.find(params[:id])
-    #@reservation.tasks = @reservation.tasks.where('reservation_id' => params[:id]).order(:priority)
-  end
+	def show
+        @reservation = Reservation.find(params[:id])
+	end
 
-  def completed
-    @reservation = Reservation.find(params[:id])
-    #@completed_tasks = @reservation.tasks.where('reservation_id' => params[:id]).order(:priority).only_deleted
+	def new
+		@reservation = Reservation.new
+	end
 
-    render 'show'
-  end
+	def edit
+		@reservation = Reservation.find(params[:id])
+	end
 
-  def new
-    @reservation = Reservation.new
-  end
+	def create
+		@reservation = Reservation.new(reservation_params)
 
-  def edit
-    @reservation = Reservation.find(params[:id])
-  end
+		if @reservation.save
+			redirect_to @reservation
+		else
+			render 'new'
+		end
+	end
 
-  def create
-    @reservation = Reservation.new(reservation_params)
+	def update
+		@reservation = Reservation.find(params[:id])
 
-    if @reservation.save
-      redirect_to @reservation
-    else
-      render 'new'
-    end
-  end
+		if @reservation.update(reservation_params)
+			redirect_to @reservation
+		else
+			render 'edit'
+		end
+	end
 
-  def update
-    @reservation = Reservation.find(params[:id])
+	def destroy
+		@reservation = Reservation.find(params[:id])
+		# tasks = @reservation.tasks.with_deleted.where('reservation_id' => params[:id])
+		# tasks.each do |task|
+		#	 task.really_destroy!
+		# end
+		@reservation.destroy
 
-    if @reservation.update(reservation_params)
-      redirect_to @reservation
-    else
-      render 'edit'
-    end
-  end
+		redirect_to reservations_path
+	end
 
-  def destroy
-    @reservation = Reservation.find(params[:id])
-    # tasks = @reservation.tasks.with_deleted.where('reservation_id' => params[:id])
-    # tasks.each do |task|
-    #   task.really_destroy!
-    # end
-    @reservation.destroy
-
-    redirect_to reservations_path
-  end
-
-  private
-    def reservation_params
-      params.require(:reservation).permit(:book_id, :user_id, :from, :to)
-    end
+	private
+		def reservation_params
+			params.require(:reservation).permit(:book_id, :user_id, :from, :to)
+		end
 end
