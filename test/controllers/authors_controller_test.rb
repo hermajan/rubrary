32315d5da9bd718beb34f07b714793f1158ce36d
Jan Hermann <hermajan@gmail.com>
2017-02-16 02:04:19 +0100

    Added tests for Author.

--- test/controllers/authors_controller_test.rb
+++ test/controllers/authors_controller_test.rb
@@ -1,8 +1,37 @@
 require 'test_helper'
 
 class AuthorsControllerTest < ActionDispatch::IntegrationTest
-	#test 'should get index' do
-	#	get authors_index_url
-	#	assert_response :success
-	#end
+	test 'should get index' do
+		get authors_path
+		assert_response :success
+	end
+
+	test 'should show author' do 
+		author = authors(:tolkien)
+		get authors_path(author)
+		assert_response :success
+	end
+
+	# test 'should create author' do
+	# 	assert_difference('Author.count') do
+	# 		post author_path, params: { author: { name: 'George Orwell' } }
+	# 	end
+	# 	assert_redirected_to authors_path(Author.last)
+	# end
+	# 
+	# test 'should update author' do
+	# 	author = authors(:kasparek)
+	# 	patch author_url(author), params: { author: { name: 'Michal' } }
+	# 	assert_redirected_to author_path(author)
+	# 	author.reload
+	# 	assert_equal 'Michal', author.name
+	# end
+	# 
+	# test 'should destroy author' do
+	# 	author = authors(:kasparek)
+	# 	assert_difference('Author.count', -1) do
+	# 		delete author_path(author) 
+	# 	end
+	# 	assert_redirected_to authors_path
+	# end
 end
--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -6,7 +6,26 @@ class AuthorTest < ActiveSupport::TestCase
 		assert_not author.save, 'Saved the author without a name'
 	end
 	
-	test "author name" do
-		assert_equal "J.R.R. Tolkien", authors(:tolkien).name
+	test 'author params' do
+		assert_equal 'J.R.R. Tolkien', authors(:tolkien).name
+	end
+	
+	test 'author create' do
+		author = Author.new
+		author.name = 'George Orwell'
+		assert author.save
+	end
+	
+	test 'author update' do
+		author = Author.find_by(id: 1)
+		assert_equal 'J.R.R. Tolkien', author.name
+		author.update(name: 'Tolkien')
+		assert_equal 'Tolkien', author.name
+	end
+	
+	test 'author delete' do
+		author = Author.find_by(id: 2)
+		author.destroy
+		assert_not_nil author.deleted_at
 	end
 end
